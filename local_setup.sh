686c6557cbe26c30409f7e865a508168d71510d3
kousheekc <kousheekc@gmail.com>
2024-06-13 10:36:53 +0200

    add machines to c0 challenge

--- local_setup.sh
+++ local_setup.sh
@@ -6,6 +6,6 @@ export RC_MQTT_KEY=randomkey
 export RC_MAGENTA=PYRO
 export RC_AUTO_START=true
 
-export REFBOX_ARGS="--cfg-mps mps/mockup_mps.yaml --cfg-challenges challenges/prod/c3.yaml --cfg-comm comm/default_comm.yaml --cfg-team team/default_team.yaml"
+export REFBOX_ARGS="--cfg-mps mps/mockup_mps.yaml --cfg-challenges challenges/prod/c0.yaml --cfg-comm comm/default_comm.yaml --cfg-team team/default_team.yaml"
 #export REFBOX_ARGS="--cfg-mps mps/festo_mps.yaml --cfg-mongodb mongodb/enable_mongodb.yaml --cfg-game game/default_game.yaml --cfg-comm comm/caros_comm.yaml --cfg-team team/default_team.yaml --cfg-challenges challenges/challenge_prod_c0_grips.yaml"
 #export REFBOX_ARGS="--cfg-mps mps/mockup_mps.yaml --cfg-game game/default_game.yaml --cfg-comm comm/default_comm.yaml --cfg-team team/default_team.yaml"
